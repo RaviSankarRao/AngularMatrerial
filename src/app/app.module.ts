278c00bc0afd3ed7710584ba27ff439de6f5736f
v-ravdas <v-ravdas@microsoft.com>
2017-08-25 14:25:20 -0700

    added header and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MdListModule } from '@angular/material';
+import { MdListModule, MdToolbarModule, MdIconModule, MdGridListModule, MdSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    MdListModule
+    MdListModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdGridListModule,
+    MdSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
